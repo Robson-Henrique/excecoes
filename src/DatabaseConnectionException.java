fc7c721ab3b889f6b62fc8cd7c5b48007071135a
Robson Henrique <111644691+Robson-Henrique@users.noreply.github.com>
2024-06-02 12:05:38 -0300

    upload das exceções

---
+++ src/DatabaseConnectionException.java
@@ -0,0 +1,10 @@
+public class DatabaseConnectionException extends RuntimeException {
+    public DatabaseConnectionException(String message) {
+        super(message);
+    }
+
+    public DatabaseConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
